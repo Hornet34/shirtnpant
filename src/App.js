a66826d7720936e0ae0531fdfb122eb0ffd5d57c
Hatim <htimdady31i@gmail.com>
2023-04-10 00:04:45 +0530

    started implemeting react router, created home page, created custom logo, started building navigation bar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
-import './categories.styles.scss';
-import CategoryMenu from './components/category-menu/category-menu.component';
-
-function App() {
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
+import Navigation from './routes/navigation/navigation.component';
+import Home from './routes/home/home.component';
+import { Routes, Route } from 'react-router-dom';
 
 
+
+const Shop = () => {
   return (
-    <CategoryMenu categories={categories} />
+    <h1>I am a Shop</h1>
+  );
+}
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+      </Route>
+
+    </Routes>
   );
 }
 
