3fa9debaba62c7f1ac13b5a9cad66f9ce19ba477
Hatim <htimdady31i@gmail.com>
2023-04-11 16:21:50 +0530

    Successfully created nav-logo, branch work complete

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../assets/shirtnpant-high-resolution-logo-color-on-transparent-background.svg';
+import { ReactComponent as Logo } from '../../assets/Untitled (1).svg';
 import './navigation.styles.scss';
 
 const Navigation = () => {
